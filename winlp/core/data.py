d45ce4fc289f56fc2c67ed4dbd984a08294bccb2
bryant <leehao90301@gmail.com>
2023-10-18 10:17:08 +0800

    [Add] 建立 DataModule 公版
    [Describe]

---
+++ winlp/core/data.py
@@ -0,0 +1,46 @@
+from typing import Optional
+
+import os
+
+import lightning.pytorch as pl
+import datasets
+import transformers
+from torch.utils.data import DataLoader
+
+
+class DataModule(pl.LightningDataModule):
+    def __init__(
+        self,
+        dataset_name: str,
+        pretrained_model_name_or_path: str,
+        max_length: int = 512,
+        batch_size: int = 16,
+        num_workers: Optional[int] = None,
+    ) -> None:
+        super().__init__()
+        self.dataset_name = dataset_name
+        self.pretrained_model_name_or_path = pretrained_model_name_or_path
+        self.max_length = max_length
+        self.batch_size = batch_size
+        self.num_workers = num_workers if num_workers is not None else os.cpu_count() / 2
+        self.tokenizer = transformers.AutoTokenizer.from_pretrained(pretrained_model_name_or_path)
+
+    def setup(self, stage: str) -> None:
+        dataset = datasets.load_dataset(path=self.dataset_name)
+        for split in dataset:
+            dataset[split] = self.process_data(dataset[split], split)
+
+        dataset.set_format(type="torch", columns=self.tokenizer.model_input_names + ["lables"])
+        self.dataset = dataset
+
+    def process_data(self, split_dataset: datasets.Dataset, split: str) -> datasets.Dataset:
+        return split_dataset
+
+    def train_dataloader(self) -> DataLoader:
+        return DataLoader(self.dataset["train"], batch_size=self.batch_size, shuffle=True, num_workers=self.num_workers)
+
+    def val_dataloader(self) -> DataLoader:
+        return DataLoader(self.dataset["validation"], batch_size=self.batch_size, num_workers=self.num_workers)
+
+    def test_dataloader(self) -> DataLoader:
+        return DataLoader(self.dataset["test"], batch_size=self.batch_size, num_workers=self.num_workers)
