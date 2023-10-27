5ba00e0c564b27fa8edd947e293f061970e83042
bryant <leehao90301@gmail.com>
2023-10-27 11:10:29 +0800

    [Add] Docstring 及移除 DataLoader num_workers 設定
    [Describe]

--- winlp/core/data.py
+++ winlp/core/data.py
@@ -15,6 +15,13 @@ LABELS = "labels"
 
 
 class DataModule(pl.LightningDataModule):
+    """
+    基於 PyTorch Lightning 的 DataModule，用於處理自然語言處理（NLP）任務數據集的模板。
+
+    封裝了從 Hugging Face 的 `datasets` 和 `transformers` 庫加載並預處理數據集的過程，使其能夠
+    方便地與 PyTorch 模型集成。
+    """
+
     def __init__(
         self,
         dataset_name: str,
@@ -23,7 +30,18 @@ class DataModule(pl.LightningDataModule):
         batch_size: int = 16,
         num_workers: Optional[int] = None,
     ) -> None:
+        """
+        初始化 DataModule。
+
+        Args:
+            dataset_name (str): 數據集的名稱。遵從 Hugging Face dataset 格式。
+            pretrained_model_name_or_path (str): 預訓練模型的名稱或路徑。
+            max_length (int, optional): 在 tokenization 過程中序列的最大長度。預設為 512。
+            batch_size (int, optional): DataLoader 中每批次加載的數據樣本數量。預設為 16。
+            num_workers (Optional[int], optional): DataLoader 用於加載數據的工作進程數。若為 None，則自動設置為機器 CPU 核心數的一半。預設為 None。
+        """
         super().__init__()
+        self.save_hyperparameters()
         self.dataset_name = dataset_name
         self.pretrained_model_name_or_path = pretrained_model_name_or_path
         self.max_length = max_length
@@ -32,6 +50,12 @@ class DataModule(pl.LightningDataModule):
         self.tokenizer = transformers.AutoTokenizer.from_pretrained(pretrained_model_name_or_path)
 
     def setup(self, stage: str) -> None:
+        """
+        加載和預處理數據集。
+
+        Args:
+            stage (str): `fit`、`validate`、`test` 或 `predict`。
+        """
         dataset = datasets.load_dataset(path=self.dataset_name)
         for split in dataset:
             dataset[split] = self.process_data(dataset[split], split)
@@ -40,13 +64,26 @@ class DataModule(pl.LightningDataModule):
         self.dataset = dataset
 
     def process_data(self, split_dataset: datasets.Dataset, split: str) -> datasets.Dataset:
-        return split_dataset
+        """
+        預處理數據集。這個方法應在子類中實現，以進行數據集的清洗、過濾、轉換等操作。
+
+        Args:
+            split_dataset (datasets.Dataset): 要進行預處理的數據集分割，例如 `train`、`validation` 或 `test`。
+            split (str): 指定當前正在處理的數據集分割部分，如 `train`、`validation` 或 `test`。
+
+        Raises:
+            NotImplementedError: 如果該方法未在子類中實現，則拋出此異常。
+
+        Returns:
+            datasets.Dataset: 預處理後的數據集分割。此數據集準備好被模型訓練或測試使用。
+        """
+        raise NotImplementedError("必須在子類中實現數據集的預處理流程。")
 
     def train_dataloader(self) -> DataLoader:
-        return DataLoader(self.dataset[TRAIN], batch_size=self.batch_size, shuffle=True, num_workers=self.num_workers)
+        return DataLoader(self.dataset[TRAIN], batch_size=self.batch_size, shuffle=True)
 
     def val_dataloader(self) -> DataLoader:
-        return DataLoader(self.dataset[VALIDATION], batch_size=self.batch_size, num_workers=self.num_workers)
+        return DataLoader(self.dataset[VALIDATION], batch_size=self.batch_size)
 
     def test_dataloader(self) -> DataLoader:
-        return DataLoader(self.dataset[TEST], batch_size=self.batch_size, num_workers=self.num_workers)
+        return DataLoader(self.dataset[TEST], batch_size=self.batch_size)
--- winlp/core/model.py
+++ winlp/core/model.py
@@ -7,9 +7,16 @@ import transformers
 import mlflow
 from torch.optim import Optimizer, AdamW
 from transformers.models.auto.auto_factory import _BaseAutoModelClass
+from lightning.pytorch.loggers import MLFlowLogger
 
 
 class Module(pl.LightningModule):
+    """
+    基於 PyTorch Lightning 的 Module，用於訓練和測試的深度學習模型模板。
+
+    主要處理包括模型初始化、度量指標配置、優化器和學習速率調度器的配置，以及訓練期間的回調函數配置。
+    """
+
     def __init__(
         self,
         downstream_model_type: Type[_BaseAutoModelClass],
@@ -21,6 +28,19 @@ class Module(pl.LightningModule):
         weight_decay: float = 1e-2,
         warmup_ratio: float = 0.0,
     ) -> None:
+        """
+        初始化 Module。
+
+        Args:
+            downstream_model_type (Type[_BaseAutoModelClass]): 下游任務模型的類型。
+            pretrained_model_name_or_path (str): 預訓練模型的名稱或路徑。
+            num_labels (int): 分類任務的標籤數量。
+            monitor (str): 要監控的指標名稱。
+            mode (str): 監控指標的模式，例如 `min`、`max`。
+            learning_rate (float, optional): 學習率。預設為 1e-3。
+            weight_decay (float, optional): 權重衰減，用於正則化和防止模型過擬合。預設為 1e-2。
+            warmup_ratio (float, optional): 預熱比例，在訓練初期學習率逐漸提升階段所佔的總訓練步驟比例。預設為 0.0。
+        """
         super().__init__()
         self.save_hyperparameters()
         self.pretrained_model_name_or_path = pretrained_model_name_or_path
@@ -36,13 +56,28 @@ class Module(pl.LightningModule):
         self.warmup_ratio = warmup_ratio
 
     def setup(self, stage: str) -> None:
+        """
+        在每個階段開始時調用，用於配置模型相關的設置。
+
+        Args:
+            stage (str): 當前的訓練階段。
+        """
         # self.logger.log_hyperparams()  # 待補 hydra model_config
         self.configure_metrics()
 
     def configure_metrics(self) -> None:
+        """
+        覆寫以配置用於訓練和驗證的度量指標。
+        """
         pass
 
     def configure_optimizers(self) -> Tuple[Sequence[Optimizer], Sequence[Dict]]:
+        """
+        配置用於訓練模型的優化器和學習率調度器。
+
+        Returns:
+            Tuple[Sequence[Optimizer], Sequence[Dict]]: 返回一個包含優化器和學習率調度器設置的元組。
+        """
         self.optimizer = AdamW(self.parameters(), lr=self.learning_rate, weight_decay=self.weight_decay)
         self.scheduler = transformers.get_linear_schedule_with_warmup(
             self.optimizer,
@@ -53,17 +88,42 @@ class Module(pl.LightningModule):
         return [self.optimizer], [scheduler]
 
     def configure_callbacks(self) -> Union[Sequence[pl.callbacks.Callback], pl.callbacks.Callback]:
+        """
+        配置訓練過程中使用的回調函數。
+
+        Returns:
+            Union[Sequence[pl.callbacks.Callback], pl.callbacks.Callback]: 一個或多個回調函數。
+        """
         calback = MLflowModelCheckpoint(logger=self.logger, monitor=self.monitor, mode=self.mode)
         return [calback]
 
 
 class MLflowModelCheckpoint(pl.callbacks.ModelCheckpoint):
-    def __init__(self, logger, **kwargs):
+    """
+    專為 MLflow 整合的 ModelCheckpoint。
+
+    主要功能是在訓練過程中選擇最佳模型檢查點，然後將該模型保存到 MLflow 中，以便進行後續的實驗追蹤和模型部署。
+    """
+
+    def __init__(self, logger: MLFlowLogger, **kwargs):
+        """
+        初始化 MLflowModelCheckpoint。
+
+        Args:
+            logger (MLFlowLogger): 用於連接 MLflow 的 logger 實例。
+        """
         super().__init__(**kwargs)
         mlflow.set_experiment(experiment_id=logger.experiment_id)
         mlflow.start_run(run_id=logger.run_id)
 
     def _save_checkpoint(self, trainer: pl.Trainer, filepath: str) -> None:
+        """
+        在訓練過程中保存模型檢查點到 MLflow。
+
+        Args:
+            trainer (pl.Trainer): PyTorch Lightning 訓練器。
+            filepath (str): 要保存的模型檢查點文件的路徑。
+        """
         mlflow.pytorch.log_model(trainer.model, "model")
 
         self._last_global_step_saved = trainer.global_step
